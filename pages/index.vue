5ef748e9dec544ffa554fbb9ade32bcef2ee8c30
pham.van.ngoc <pham.van.ngoc@B121645-mb.local>
2020-05-07 16:53:48 +0700

    update blog

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,24 @@
     <homepage-banner :banner="banner"/>
     <!-- Slices block component -->
     <slices-block :slices="slices"/>
+    <!-- Check blog posts exist -->
+    <div v-if="posts.length !== 0" class="blog-main">
+      <!-- Template for blog posts -->
+      <section v-for="post in posts" :key="post.id" v-bind:post="post" class="blog-post">
+        <!-- Here :post="post" passes the data to the component -->
+        <blog-widget :post="post"></blog-widget>
+      </section>
+    </div>
+    <!-- If no blog posts return message -->
+    <div v-else class="blog-main">
+      <p>No Posts published at this time.</p>
+    </div>
   </section>
 </template>
 
 <script>
 // Imports for all components
+import BlogWidget from '~/components/BlogWidget.vue'
 import HomepageBanner from '~/components/HomepageBanner.vue'
 import SlicesBlock from '~/components/SlicesBlock.vue'
 
@@ -17,11 +30,12 @@ export default {
   layout: 'homepage',
   components: {
     HomepageBanner,
-    SlicesBlock
+    SlicesBlock,
+    BlogWidget
   },
   head () {
     return {
-      title: 'Prismic Nuxt.js Multi Page Website',
+      title: 'Ngoc blog',
     }
   },
   async asyncData({ $prismic, error }) {
@@ -29,9 +43,16 @@ export default {
       // Query to get the home page content
       const homepage = (await $prismic.api.getSingle('homepage')).data
 
+      // Query to get posts content to preview
+      const blogPosts = await $prismic.api.query(
+        $prismic.predicates.at("document.type", "post"),
+        { orderings : '[my.post.date desc]' }
+      )
+
       return {
         // Page content
         banner: homepage.homepage_banner[0],
+        posts: blogPosts.results,
         // Set slices as variable
         slices: homepage.page_content
       }
@@ -41,3 +62,51 @@ export default {
   },
 }
 </script>
+
+<style lang="sass" scoped>
+.home
+  max-width: 700px
+  margin: auto
+  text-align: center
+  .blog-avatar
+    height: 140px
+    width: 140px
+    border-radius: 50%
+    background-position: center
+    background-size: cover
+    margin: 1em auto
+  .blog-description
+    font-size: 18px
+    color: #9A9A9A
+    line-height: 30px
+    margin-bottom: 3rem
+    padding-bottom: 3rem
+    font-family: 'Lato', sans-serif
+    border-bottom: 1px solid #DADADA
+
+.blog-main
+  max-width: 700px
+  margin: auto
+  text-align: left
+  &.single img
+    width: 100%
+    height: auto
+  &.single a
+    text-decoration: none
+    background: -webkit-linear-gradient(top, rgba(0, 0, 0, 0) 75%, rgba(0, 0, 0, 0.8) 75%)
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 75%, rgba(0, 0, 0, 0.8) 75%)
+    background-repeat: repeat-x
+    background-size: 2px 2px
+    background-position: 0 23px
+
+.blog-post
+  margin: 0
+  margin-bottom: 3rem
+
+@media (max-width: 767px)
+  .home
+    padding: 0 20px
+  .blog-main
+    padding: 0
+    font-size: 18px
+</style>
